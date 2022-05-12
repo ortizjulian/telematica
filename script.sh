9f97149d088eafa43e694d156a54496e767f5c3e
ortizjulian <101840232+ortizjulian@users.noreply.github.com>
2022-05-12 16:45:59 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,9 +3,10 @@ sudo apt install docker-compose -y
 git clone https://github.com/ortizjulian/telematica.git
 cd telematica
 mkdir static
+mkdir static/css
 mkdir templates
 mv index.html templates
-mv style.css static
+mv style.css static/css
 sudo docker pull python
 sudo docker build -t final:0 .
 sudo docker run -d -p 80:80 final:0 python3 /myapp/app.py
